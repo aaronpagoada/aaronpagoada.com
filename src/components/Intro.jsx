3bde852164bb98d0dc2e6c1ae8fbf8a9cafa2d19
AP <74792335+aaronpagoada@users.noreply.github.com>
2023-11-26 12:31:29 -0500

    Edited title cycle

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -11,11 +11,7 @@ function Intro(){
     <div className="intro">
       <h1>Aaron Pagoada</h1>
       <div className="intro-text">
-        <span className="title active">Software engineer </span>
-        <span className="title inactive">Climber </span>
-        <span className="title inactive">Hiker </span>
-        <span className="title inactive">Soccer player </span>
-        <span className="title inactive">Builder </span>
+        <h2 id="title" className="active">Software engineer</h2>
       </div>
     </div>
   )
--- src/js/titleCycle.js
+++ src/js/titleCycle.js
@@ -1,19 +1,14 @@
 import sleep from "./sleep";
 
 async function titleCycle(){
-  const titles = document.querySelectorAll('span.title');
+  const title = document.getElementById('title')
+  const titles = ['Software engineer', 'Climber', 'Hiker', 'Instructor', 'Builder']
 
   let i = 1
 
   while(i < 101){
-    let j = i % titles.length
     await sleep(5000)
-    if(j === 0){
-      titles[titles.length - 1].classList.replace('active', 'inactive')
-    } else {
-      titles[j - 1].classList.replace('active', 'inactive')
-    }
-    titles[j].classList.replace('inactive', 'active')
+    title.textContent = titles[i % 5]
     i++
   }
 }
