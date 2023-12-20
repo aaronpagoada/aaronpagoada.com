d92f6a04f85db09aa03e05b8662069b3a7289081
AP <74792335+aaronpagoada@users.noreply.github.com>
2023-12-20 02:11:00 -0500

    Reworked work data

--- src/components/WorkTimeline.jsx
+++ src/components/WorkTimeline.jsx
@@ -4,50 +4,38 @@ import TerminalIcon from '@mui/icons-material/Terminal';
 import InsightsIcon from '@mui/icons-material/Insights';
 import GroupsIcon from '@mui/icons-material/Groups';
 import TimelineCard from "./TimelineCard";
+const data = require("../data/Work.json")
 
 function WorkTimeline(){
   return(
     <Timeline position="alternate">
-      <TimelineCard 
-        time="2021"
-        activity="Software Engineer Intern"
-        location="WatchWire"
-        expandedTime="Jun 2021 - Aug 2021"
-        expandedLocation="New York, NY"
-        icon={<TerminalIcon />}
-      />
-      <TimelineCard 
-        time="2022"
-        activity="Computer Science & Mathematics Department Peer Mentor"
-        location="John Jay College"
-        expandedTime="Mar 2022 - Jun 2022"
-        expandedLocation="New York, NY"
-        icon={<GroupsIcon />}
-      />
-      <TimelineCard 
-        time="2022"
-        activity="Software Engineer Intern"
-        location="Bowery Farming"
-        expandedTime="Jun 2022 - Aug 2022"
-        expandedLocation="New York, NY"
-        icon={<TerminalIcon />}
-      />
-      <TimelineCard 
-        time="2023"
-        activity="Software Engineer Intern"
-        location="GameChanger"
-        expandedTime="Jun 2023 - Aug 2023"
-        expandedLocation="New York, NY"
-        icon={<TerminalIcon />}
-      />
-      <TimelineCard 
-        time="2024"
-        activity="Data Intern"
-        location="Vertex Pharmaceuticals"
-        expandedTime="May 2024 - Aug 2024"
-        expandedLocation="Boston, MA"
-        icon={<InsightsIcon />}
-      />
+      {data.work.map((experience) => {
+        let workIcon;
+        
+        switch (experience.icon) {
+          case "insights":
+            workIcon = <InsightsIcon />
+            break
+          case "groups":
+            workIcon = <GroupsIcon />
+            break
+          case "terminal":
+            workIcon = <TerminalIcon />
+            break
+        }
+
+        return (
+          <TimelineCard 
+            activity={experience.position}
+            location={experience.company}
+            time={experience.time}
+            expandedTime={experience.expandedTime}
+            expandedLocation={experience.location}
+            expandedDetails={experience.details}
+            icon={workIcon}
+          />
+        )
+      })}
     </Timeline>
   )
 }
