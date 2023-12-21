fe0855aee789671f286e0b0e5e8204a244683efa
AP <74792335+aaronpagoada@users.noreply.github.com>
2023-12-21 01:06:45 -0500

    Added project cards

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../component-styles/ProjectCard.css"
+
+function ProjectCard({ name, description, languages, photoPath }){
+  return (
+    <div className="project-card">
+
+    </div>
+  )
+}
+
+export default ProjectCard
