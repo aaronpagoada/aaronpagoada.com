6b392609e57616cf17df9f8e7c2ccf3d3b860d5f
AP <74792335+aaronpagoada@users.noreply.github.com>
2023-12-22 18:40:29 -0500

    Added project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import { Typography } from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import PythonLogo from "../img/Python.png"
+import JavascriptLogo from "../img/javascript.png"
 import "../component-styles/ProjectCard.css"
 
-function ProjectCard({ name, description, languages, photoPath }){
+function ProjectCard({ name, description, languages, photo }){
+  let languageIcon;
+
+  switch(languages){
+    case "python":
+      languageIcon = PythonLogo
+      break
+    case "javascript":
+      languageIcon = JavascriptLogo
+      break
+  }
+
   return (
     <div className="project-card">
-
+      <img src={photo}/>
+      <div className="project-info-box">
+        <div className="project-info-text">
+          <Typography color="white" variant="h5">{name}</Typography>
+          <Typography color="white" variant="caption">{description}</Typography>
+        </div>
+        <div className="project-info-logos">  
+          <img src={languageIcon} />
+          <GitHubIcon sx={{ color: "white"}}/>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 import "../component-styles/Projects.css"
+import PizzaRat from "../img/PizzaRat.png"
 
 function Projects(){
   return(
@@ -9,8 +10,15 @@ function Projects(){
         Ut id ante justo. Sed elementum eleifend nulla nec ullamcorper. Praesent porta eros vitae neque ultrices pharetra. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas porttitor neque at hendrerit congue. Fusce risus turpis, volutpat sit amet elit at, pharetra dapibus lectus. Fusce eleifend diam varius, mollis mauris a, pulvinar sapien. Duis id dui maximus, convallis urna id, cursus ligula. Cras turpis eros, vulputate at aliquam in, facilisis tempor nulla. Praesent in quam iaculis, suscipit orci quis, auctor diam. Aenean id ex vel orci placerat tincidunt. Sed sagittis metus ut vestibulum aliquam.
       </div>
       <div className="projects-cards">
-        <ProjectCard />
-        <ProjectCard />
+        <ProjectCard 
+          name="Pizza Rat"
+          description="An MTA-based Discord bot"
+          languages="python"
+          photo={PizzaRat}  
+        />
+        <ProjectCard 
+          languages="javascript"
+        />
         <ProjectCard />
         <ProjectCard />
       </div>
