0b8ad4925a1f200f3c5d4cddc7331dd58d14b1f6
AP <74792335+aaronpagoada@users.noreply.github.com>
2023-12-23 01:43:11 -0500

    Added project photos

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,30 +3,34 @@ import { Typography } from "@mui/material";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import PythonLogo from "../img/Python.png"
 import JavascriptLogo from "../img/javascript.png"
+import ReactLogo from "../img/logo192.png"
 import "../component-styles/ProjectCard.css"
 
-function ProjectCard({ name, description, languages, photo }){
+function ProjectCard({ name, description, language, photo }){
   let languageIcon;
 
-  switch(languages){
+  switch(language){
     case "python":
       languageIcon = PythonLogo
       break
     case "javascript":
       languageIcon = JavascriptLogo
       break
+    case "react":
+      languageIcon = ReactLogo
+      break
   }
 
   return (
     <div className="project-card">
-      <img src={photo}/>
+      <img src={photo} alt={photo}/>
       <div className="project-info-box">
         <div className="project-info-text">
           <Typography color="white" variant="h5">{name}</Typography>
           <Typography color="white" variant="caption">{description}</Typography>
         </div>
         <div className="project-info-logos">  
-          <img src={languageIcon} />
+          <img src={languageIcon} alt={language}/>
           <GitHubIcon sx={{ color: "white"}}/>
         </div>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
-import "../component-styles/Projects.css"
 import PizzaRat from "../img/PizzaRat.png"
+import PagoadaDotCom from "../img/Website.png"
+import DoctorHelp from "../img/DoctorHelp.png"
+import Watson from "../img/Watson.png"
+import "../component-styles/Projects.css"
+const data = require("../data/Projects.json")
 
 function Projects(){
   return(
@@ -10,17 +14,33 @@ function Projects(){
         Ut id ante justo. Sed elementum eleifend nulla nec ullamcorper. Praesent porta eros vitae neque ultrices pharetra. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas porttitor neque at hendrerit congue. Fusce risus turpis, volutpat sit amet elit at, pharetra dapibus lectus. Fusce eleifend diam varius, mollis mauris a, pulvinar sapien. Duis id dui maximus, convallis urna id, cursus ligula. Cras turpis eros, vulputate at aliquam in, facilisis tempor nulla. Praesent in quam iaculis, suscipit orci quis, auctor diam. Aenean id ex vel orci placerat tincidunt. Sed sagittis metus ut vestibulum aliquam.
       </div>
       <div className="projects-cards">
-        <ProjectCard 
-          name="Pizza Rat"
-          description="An MTA-based Discord bot"
-          languages="python"
-          photo={PizzaRat}  
-        />
-        <ProjectCard 
-          languages="javascript"
-        />
-        <ProjectCard />
-        <ProjectCard />
+        {data.projects.map((project) => {
+          let projectImage;
+
+          switch(project.photo){
+            case "pizzarat":
+              projectImage = PizzaRat
+              break
+            case "pagoadadotcom":
+              projectImage = PagoadaDotCom
+              break
+            case "doctorhelp":
+              projectImage = DoctorHelp
+              break
+            case "watson":
+              projectImage = Watson
+              break
+          }
+
+          return (
+            <ProjectCard 
+              name={project.name}
+              description={project.description}
+              language={project.language}
+              photo={projectImage}
+            />
+          )
+        })}
       </div>
     </div>
   )
