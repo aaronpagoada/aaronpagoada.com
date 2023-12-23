54f5eef3d645a84b68ebde616f08eb810e7f0755
AP <74792335+aaronpagoada@users.noreply.github.com>
2023-12-23 15:35:56 -0500

    Added work photos

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { Typography } from "@mui/material";
 import WorkTimeline from "./WorkTimeline";
+import Bowery from "../img/bowery.jpeg"
+import GC from "../img/gc.jpeg"
 import "../component-styles/Work.css"
 
 function Work(){
@@ -7,6 +10,16 @@ function Work(){
     <div className="work">
       <h3>Work</h3>
       <WorkTimeline />
+      <div className="work-photos">
+        <div>
+          <img src={Bowery} alt="Bowery" />
+          <Typography variant="caption">Aug 2022 at Bowery Farming, first on left</Typography>
+        </div>
+        <div>
+          <img src={GC} alt="GC" />
+          <Typography variant="caption">Jun 2023 at GameChanger, third on left</Typography>
+        </div>
+      </div>
     </div>
   )
 }
