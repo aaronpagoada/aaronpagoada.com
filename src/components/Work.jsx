6d906fe31dd0fae86030aacef9dcdabfa1ae388b
Aaron Pagoada <74792335+aaronpagoada@users.noreply.github.com>
2023-12-25 01:47:27 -0500

    Edited work captions

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -13,11 +13,11 @@ function Work(){
       <div className="work-photos">
         <div>
           <img src={Bowery} alt="Bowery" />
-          <Typography variant="caption">Aug 2022 at Bowery Farming, first on left</Typography>
+          <Typography variant="caption">Aug 2022, Bowery Farming summer interns, first on left</Typography>
         </div>
         <div>
           <img src={GC} alt="GC" />
-          <Typography variant="caption">Jun 2023 at GameChanger, third on left</Typography>
+          <Typography variant="caption">Jun 2023, GameChanger summer interns, third on left</Typography>
         </div>
       </div>
     </div>
