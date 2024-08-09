6c30f358c22ab099a0f4ad1fd1739eb97bc389b8
AP <74792335+aaronpagoada@users.noreply.github.com>
2024-08-09 13:02:52 -0400

    Remove home menu

--- src/components/MainDrawer.jsx
+++ src/components/MainDrawer.jsx
@@ -1,46 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { IconButton, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 import '../component-styles/MainDrawer.css';
 
 function MainDrawer(){
-  const [isOpen, setIsOpen] = useState(false)
-
   return (
     <div>
-      <IconButton
-        onClick={() => setIsOpen(!isOpen)}
-        aria-label="open-links"
-      >
-        <MenuIcon 
-          fontSize="large"
-          sx={{ color: "#ffffff"}}
-        />
-      </IconButton>
-			{isOpen && 
-				<div className="drawer">
-					<div className="drawer-links">
-						<List>
-							{['About', 'Projects', 'Education', 'Work'].map((linkText) => (
-								<ListItem key={linkText} disablePadding>
-									<ListItemButton>
-										<Link 
-											to={`/${linkText.toLowerCase()}`}
-											style={{ 
-												textDecoration: 'none',
-												color: '#000000',
-											}}  
-										>
-											<ListItemText primary={linkText} />
-										</Link>
-									</ListItemButton>
-								</ListItem>
-							))}
-						</List>	
-					</div>
-				</div>
-			}
+      <div className="drawer">
+        <div className="drawer-links">
+          <List>
+            {['About', 'Projects', 'Education', 'Work'].map((linkText) => (
+              <ListItem key={linkText} disablePadding>
+                <ListItemButton>
+                  <Link 
+                    to={`/${linkText.toLowerCase()}`}
+                    style={{ 
+                      textDecoration: 'none',
+                      color: '#000000',
+                    }}  
+                  >
+                    <ListItemText primary={linkText} />
+                  </Link>
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>	
+        </div>
+      </div>
     </div>
   )
 }
