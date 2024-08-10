2b486bb6504a2f4c99e333696043cbca3d7b5636
AP <74792335+aaronpagoada@users.noreply.github.com>
2024-08-09 23:08:19 -0400

    pdated homepage

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,20 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link, Outlet } from "react-router-dom";
-import MainDrawer from "../MainDrawer";
 import logo from "../../img/apblock.png";
 import '../../component-styles/MainLayout.css';
 
 function MainLayout() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-  let big = windowWidth > 1320 ? true : false;
-  useEffect(() => {
-    function handleResize(){
-      setWindowWidth(window.innerWidth)
-      windowWidth > 1320 ? big = true : big = false
-    }
-    window.addEventListener('resize', handleResize)
-  }, [])
-
   return (
     <div className="main-layout">
       <div className="navbar">
@@ -26,19 +15,12 @@ function MainLayout() {
               height="45px"
             />
           </Link>
-          {big &&
-            <div className="links">
-              <Link to="/about">About</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/education">Education</Link>
-              <Link to="/work">Work</Link>
-            </div>
-          }
-          {!big &&
-            <div className="button">
-              <MainDrawer />
-            </div>
-          } 
+          <div className="links">
+            <Link to="/about">About</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/education">Education</Link>
+            <Link to="/work">Work</Link>
+          </div>
         </nav>
       </div>
       <Outlet />
