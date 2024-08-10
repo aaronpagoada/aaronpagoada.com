f9100e7f9cc19392b33a723d42ce5d20d9f30b9a
AP <74792335+aaronpagoada@users.noreply.github.com>
2024-08-10 00:34:10 -0400

    Updated appearance

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,37 +7,39 @@ import "../component-styles/About.css";
 
 function About(){
   return(
-    <div className="about">
-      <h3>About</h3>
-      <div className="about-flex-container">
-        <div className="image-box">
-          <img id="about-image" src={require('../img/BoweryInterns-16.jpg')} alt="Bowery Headshot" />
+    <div className="about-page">
+      <div className="about">
+        <h3>About</h3>
+        <div className="about-flex-container">
+          <div className="image-box">
+            <img id="about-image" src={require('../img/BoweryInterns-16.jpg')} alt="Bowery Headshot" />
+          </div>
+          <div className="about-text">
+            <Typography variant="body1">
+              Hi, I'm Aaron!
+            </Typography>
+            <br />
+            <Typography variant="body1">
+              I recently graduated from Macaulay Honors College at John Jay College where I studied Computer Science and Information Security.
+            </Typography>
+            <br />
+            <Typography variant="body1">
+              In the past, I've interned on software teams at <Link to="https://vrtx.com">Vertex Pharmaceuticals</Link>, <Link to="https://gc.com/">GameChanger</Link>, <Link to="https://bowery.co/">Bowery Farming</Link>, and <Link to="https://watchwire.ai/">WatchWire</Link>. I've built modern web pages and created components to design specs. I've also integrated third-party app APIs with existing infrastructure and cleaned up data pipelines. I also worked in John Jay's Computer Science and Mathematics department as a peer mentor where I advised students every week on how to excel in school and establish the foundations for their future technical careers.
+            </Typography>
+            <br />
+            <Typography variant="body1">
+              I am a rock climbing coach and instructor at Brooklyn Boulders. I enjoy teaching both adults and children how to improve in the sport.
+            </Typography>
+            <br/>
+            <IconButton href="https://www.linkedin.com/in/aaronpagoada">
+              <LinkedInIcon sx={{ color: "black" }}/>
+            </IconButton>
+            <IconButton href="https://github.com/aaronpagoada">
+              <GitHubIcon sx={{ color: "black" }}/>
+            </IconButton>
+          </div>
         </div>
-        <div className="about-text">
-          <Typography variant="body1">
-            Hi, I'm Aaron!
-          </Typography>
-          <br />
-					<Typography variant="body1">
-            I recently graduated from Macaulay Honors College at John Jay College where I studied Computer Science and Information Security.
-          </Typography>
-          <br />
-          <Typography variant="body1">
-            In the past, I've interned on software teams at <Link to="https://vrtx.com">Vertex Pharmaceuticals</Link>, <Link to="https://gc.com/">GameChanger</Link>, <Link to="https://bowery.co/">Bowery Farming</Link>, and <Link to="https://watchwire.ai/">WatchWire</Link>. I've built modern web pages and created components to design specs. I've also integrated third-party app APIs with existing infrastructure and cleaned up data pipelines. I also worked in John Jay's Computer Science and Mathematics department as a peer mentor where I advised students every week on how to excel in school and establish the foundations for their future technical careers.
-          </Typography>
-          <br />
-          <Typography variant="body1">
-            I am a rock climbing coach and instructor at Brooklyn Boulders. I enjoy teaching both adults and children how to improve in the sport.
-          </Typography>
-					<br/>
-					<IconButton href="https://www.linkedin.com/in/aaronpagoada">
-            <LinkedInIcon sx={{ color: "black" }}/>
-          </IconButton>
-					<IconButton href="https://github.com/aaronpagoada">
-            <GitHubIcon sx={{ color: "black" }}/>
-          </IconButton>
-        </div>
-      </div>
+      </div>  
     </div>
   )
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -8,21 +8,23 @@ import "../component-styles/Work.css"
 
 function Work(){
   return (
-    <div className="work">
-      <h3>Work</h3>
-      <WorkTimeline />
-      <div className="work-photos">
-        <div>
-          <img src={Bowery} alt="Bowery" />
-          <Typography variant="caption">Aug 2022, Bowery Farming summer interns, first on left</Typography>
-        </div>
-        <div>
-          <img src={GC} alt="GameChanger" />
-          <Typography variant="caption">Jun 2023, GameChanger summer interns, third on left</Typography>
-        </div>
-        <div>
-          <img src={Vertex} alt="Vertex" />
-          <Typography variant="caption">May 2024, Vertex summer interns, third row, fourth on right</Typography>
+    <div className="work-page">
+      <div className="work">
+        <h3>Work</h3>
+        <WorkTimeline />
+        <div className="work-photos">
+          <div>
+            <img src={Bowery} alt="Bowery" />
+            <Typography variant="caption">Aug 2022, Bowery Farming summer interns, first on left</Typography>
+          </div>
+          <div>
+            <img src={GC} alt="GameChanger" />
+            <Typography variant="caption">Jun 2023, GameChanger summer interns, third on left</Typography>
+          </div>
+          <div>
+            <img src={Vertex} alt="Vertex" />
+            <Typography variant="caption">May 2024, Vertex summer interns, third row, fourth on right</Typography>
+          </div>
         </div>
       </div>
     </div>
