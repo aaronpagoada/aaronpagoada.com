2377ba59ac216dffd312cef89b5d2cb3d98c8ca9
Aaron Pagoada <74792335+aaronpagoada@users.noreply.github.com>
2024-08-11 12:02:09 -0400

    Edited about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,16 +19,20 @@ function About(){
               Hi, I'm Aaron!
             </Typography>
             <br />
+						<Typography variant="body1">
+              I am a passionate software engineer with a strong background in web development.
+            </Typography>
+						<br />
+            <Typography variant="body1">
+              I've worked on software teams at <Link to="https://vrtx.com">Vertex Pharmaceuticals</Link>, <Link to="https://gc.com/">GameChanger</Link>, <Link to="https://bowery.co/">Bowery Farming</Link>, and <Link to="https://watchwire.ai/">WatchWire</Link>.
+            </Typography>
+            <br />
             <Typography variant="body1">
               I recently graduated from Macaulay Honors College at John Jay College where I studied Computer Science and Information Security.
             </Typography>
             <br />
             <Typography variant="body1">
-              In the past, I've interned on software teams at <Link to="https://vrtx.com">Vertex Pharmaceuticals</Link>, <Link to="https://gc.com/">GameChanger</Link>, <Link to="https://bowery.co/">Bowery Farming</Link>, and <Link to="https://watchwire.ai/">WatchWire</Link>. I've built modern web pages and created components to design specs. I've also integrated third-party app APIs with existing infrastructure and cleaned up data pipelines. I also worked in John Jay's Computer Science and Mathematics department as a peer mentor where I advised students every week on how to excel in school and establish the foundations for their future technical careers.
-            </Typography>
-            <br />
-            <Typography variant="body1">
-              I am a rock climbing coach and instructor at Brooklyn Boulders. I enjoy teaching both adults and children how to improve in the sport.
+              I love being out in nature and enjoy bringing others to explore with me.
             </Typography>
             <br/>
             <IconButton href="https://www.linkedin.com/in/aaronpagoada">
