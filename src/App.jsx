ddf41577c0b5cdc4693079008a9f4ed82f53fafd
AP <74792335+aaronpagoada@users.noreply.github.com>
2025-02-21 12:58:58 -0500

    Vite-ified

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ThemeToggle from './components/ThemeToggle'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <ThemeToggle />
+    </>
+  )
+}
+
+export default App
