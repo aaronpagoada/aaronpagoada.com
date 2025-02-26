90059e1aa5d3651b2e45bab60c558f4650fee022
AP <74792335+aaronpagoada@users.noreply.github.com>
2025-02-25 23:25:26 -0500

    Updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,15 @@ function About() {
     <div className="md:pr-24 text-gray-800 dark:text-white">
       <h1 className="text-xl font-bold">About</h1>
       <p className="pb-8 md:text-xl">Hi, I'm Aarón. I am a software engineer with a focus in web development.</p>
-      <p className="pb-8 md:text-xl">Still at the dawn of my career, I am fortunate to have contributed to teams at <a className="text-vertex-2 dark:text-vertex" href="https://vrtx.com/">Vertex Pharmaceuticals</a>, <a className="text-gc-2 dark:text-gc" href="https://gc.com/">GameChanger</a>, <a className="text-bowery-2 dark:text-bowery" href="https://bowery.co/">Bowery Farming</a>, and <a className="text-watchwire-2 dark:text-watchwire" href="https://watchwire.ai/">WatchWire</a>. I've done a little bit of everything, ranging from shipping new pages to design specs and establishing new authentication methods, to writing monitoring scripts and consolidating databases. </p>
+      <p className="pb-8 md:text-xl">
+        Fueled by an aspiration to build beautiful experiences, I am fortunate to have contributed to teams at
+        <a className="text-vertex-2 dark:text-vertex" href="https://vrtx.com/"> Vertex Pharmaceuticals</a>,
+        <a className="text-gc-2 dark:text-gc" href="https://gc.com/"> GameChanger</a>,
+        <a className="text-bowery-2 dark:text-bowery" href="https://bowery.co/"> Bowery Farming</a>, and
+        <a className="text-watchwire-2 dark:text-watchwire" href="https://watchwire.ai/"> WatchWire</a>.
+        I've shipped new pages to design specs, established new authentication methods, written monitoring scripts and peformed database housekeeping. </p>
       <p className="pb-8 md:text-xl">I am Houston-born and NYC-raised with Honduran roots. </p>
+      <p className="pb-8 md:text-xl">Beyond the keyboard, I love to get outdoors and explore. I am an avid rock climber and hiker with plenty of adventures to tell around a campfire. </p>
     </div >
   )
 }
