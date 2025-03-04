8ba9bb5c2981f00d3af7f0d9493184978b5755fd
AP <74792335+aaronpagoada@users.noreply.github.com>
2025-03-04 00:04:53 -0500

    Added more photos

--- src/components/Scroller.jsx
+++ src/components/Scroller.jsx
@@ -1,25 +1,15 @@
 function Scroller({ images, title }) {
-  //const items = new Array(length)
-
-  //for (let i = 0; i < items.length; i++) {
-  //let randomColor = Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
-  //items[i] = randomColor
-  //}
-
-  console.log(images)
-
   return (
-    <div className="pb-8 text-gray-800 dark:text-white overflow-auto whitespace-nowrap">
+    <div className="pb-8">
       <h1 className="text-xl font-bold pb-4">{title}</h1>
-
-      {images.map(image => (
-
-        <div className="mr-8 p-1 border border-solid inline-block h-54 w-72">
-          <img className="object-cover h-full" src={image} />
-        </div>
-      ))
-      }
-    </div >
+      <div className="pb-4 text-gray-800 dark:text-white overflow-auto whitespace-nowrap">
+        {images.map(image => (
+          <div className="mr-8 p-1 border border-solid inline-block h-54 w-72">
+            <img className="object-cover h-full" src={image} />
+          </div>
+        ))}
+      </div >
+    </div>
   )
 }
 
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,19 +2,15 @@ import Header from "../components/Header";
 import About from "../components/About";
 import Footer from "../components/Footer";
 import Scroller from "../components/Scroller";
-
-import shs from "../assets/img/soccerhs.jpg"
-import shs2 from "../assets/img/soccerhs2.jpg"
+import galleryImages from "../utils/galleryImages";
 
 function AboutPage() {
-  const images = [shs, shs2]
-
   return (
     <div className='bg-white dark:bg-black px-4 md:px-20 min-h-screen'>
       <Header />
       <About />
       <Footer />
-      <Scroller images={images} title="Gallery" />
+      <Scroller images={galleryImages} title="Gallery" />
     </div>
   )
 }
